0d5df30d4f2520e69967a7cf3352751c66dc9edf
RachataKH <Toon2574725@gmail.com>
2025-07-21 10:57:39 +0700

    function Components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
 import React from 'react'
-import './style.css'
+//import './style.css'
+
+import { Header,Content,Footer } from './func-components';
 
 function App() {
+
+  return([<Header/>,<Content/>,<Footer/>]
+  )
   return (
     <>
-      <h2>DeveloperThai.com</h2>
-      <div className='title'>React & React Native</div>
+      <Header/>
+      <Content/>
+      <Footer/>
     </>
   )
-  const divStyle={
-    color:'red',
-    backgroundColor:'powderblue',
-    fontSize:'larger',
-    padding:'3px',
-  }
-  return<div style={divStyle}>Hello React</div>
+
+
+  // return (
+  //   <>
+  //     <h2>DeveloperThai.com</h2>
+  //     <div className='title'>React & React Native</div>
+  //   </>
+  // )
+  // const divStyle={
+  //   color:'red',
+  //   backgroundColor:'powderblue',
+  //   fontSize:'larger',
+  //   padding:'3px',
+  // }
+  // return<div style={divStyle}>Hello React</div>
 }
 
 export default App;
