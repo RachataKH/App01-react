22becd9467a2025072f6a66d82e45c89b44e3b27
RachataKH <Toon2574725@gmail.com>
2025-07-21 11:35:32 +0700

    class-components

--- src/App.js
+++ src/App.js
@@ -4,10 +4,17 @@ import React from 'react'
 //import './style.css'
 
 import { Header,Content,Footer } from './func-components';
+import Calendar from './class-components';
 
 function App() {
 
-  return([<Header/>,<Content/>,<Footer/>]
+  return(
+    <>
+      <Header/>
+      <p><center><Calendar/></center></p>
+      <Content/>
+      <Footer/>
+    </>
   )
   return (
     <>
