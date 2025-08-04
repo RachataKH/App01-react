d1f101b8618c62bafe800003be79ac23fb1e4962
RachataKH <Toon2574725@gmail.com>
2025-08-04 09:55:10 +0700

    error in class components
    error in class components

--- src/App.js
+++ src/App.js
@@ -1,43 +1,8 @@
-//import logo from './logo.svg';
-//import './App.css';
 import React from 'react'
-//import './style.css'
-
-import { Header,Content,Footer } from './func-components';
-import Calendar from './class-components';
+import Button from './class-components';
 
 function App() {
-
-  return(
-    <>
-      <Header/>
-      <p><center><Calendar/></center></p>
-      <Content/>
-      <Footer/>
-    </>
-  )
-  return (
-    <>
-      <Header/>
-      <Content/>
-      <Footer/>
-    </>
-  )
-
-
-  // return (
-  //   <>
-  //     <h2>DeveloperThai.com</h2>
-  //     <div className='title'>React & React Native</div>
-  //   </>
-  // )
-  // const divStyle={
-  //   color:'red',
-  //   backgroundColor:'powderblue',
-  //   fontSize:'larger',
-  //   padding:'3px',
-  // }
-  // return<div style={divStyle}>Hello React</div>
+  return <Button/>
 }
 
 export default App;
